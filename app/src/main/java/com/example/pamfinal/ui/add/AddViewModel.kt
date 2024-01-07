b414e1028591f7a985491bade696c0a6a7513d9f
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-01-07 23:11:15 +0700

    createAddViewModel Pendaftar

--- app/src/main/java/com/example/pamfinal/ui/add/AddViewModel.kt
+++ app/src/main/java/com/example/pamfinal/ui/add/AddViewModel.kt
@@ -5,8 +5,19 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.pamfinal.data.PendaftarRepository
+import com.example.pamfinal.ui.AddEvent
+import com.example.pamfinal.ui.AddUIState
+import com.example.pamfinal.ui.toPendaftar
 
-class AddViewModel(private val kontakRepository: PendaftarRepository) : ViewModel() {
+class AddViewModel(private val pendaftarRepository: PendaftarRepository) : ViewModel(){
+    var addUIState by mutableStateOf(AddUIState())
+        private set
 
+    fun updateAddUIState(addEvent: AddEvent){
+        addUIState = AddUIState(addEvent = addEvent)
+    }
+
+    suspend fun addPendaftar(){
+        pendaftarRepository.save(addUIState.addEvent.toPendaftar())
+    }
 }
-
