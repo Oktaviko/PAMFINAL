4b92bf8a820d157b7ce67457082a38ccb9950627
NandaLemon <rizkynanda010703@gmail.com>
2024-01-07 13:49:49 +0700

    add object in destinasinavigasi in AddScreen

---
+++ app/src/main/java/com/example/pamfinal/ui/add/AddScreen.kt
@@ -0,0 +1,8 @@
+package com.example.pamfinal.ui.add
+
+import com.example.pamfinal.navigation.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Customer"
+}
