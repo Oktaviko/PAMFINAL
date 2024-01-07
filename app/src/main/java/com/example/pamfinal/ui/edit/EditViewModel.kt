c29d5853f67d20b8e7c24ea6d7f2e61bb832a76c
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-01-08 01:21:23 +0700

    lengkapi editviewmodel pendaftar

--- app/src/main/java/com/example/pamfinal/ui/edit/EditViewModel.kt
+++ app/src/main/java/com/example/pamfinal/ui/edit/EditViewModel.kt
@@ -5,8 +5,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pamfinal.data.PendaftarRepository
+import com.example.pamfinal.ui.AddEvent
 import com.example.pamfinal.ui.AddUIState
+import com.example.pamfinal.ui.toPendaftar
+import com.example.pamfinal.ui.toUIStatePendaftar
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class EditViewModel (
     savedStateHandle: SavedStateHandle,
@@ -16,5 +23,23 @@ class EditViewModel (
     var pendaftarUiState by mutableStateOf(AddUIState())
         private set
 
+    private val pendaftarId: String = checkNotNull(savedStateHandle[EditDestination.pendaftarId])
 
+    init{
+        viewModelScope.launch {
+            pendaftarUiState =
+                repository.getPendaftarById(pendaftarId)
+                    .filterNotNull()
+                    .first()
+                    .toUIStatePendaftar()
+        }
+    }
+
+    fun updateUIState(addEvent: AddEvent){
+        pendaftarUiState = pendaftarUiState.copy(addEvent = addEvent)
+    }
+
+    suspend fun updatePendaftar(){
+        repository.update(pendaftarUiState.addEvent.toPendaftar())
+    }
 }
