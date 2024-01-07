83efe58aec1fc3d118412043debffffbb11142ce
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-01-07 22:26:39 +0700

    tambah column di function EntryBody

--- app/src/main/java/com/example/pamfinal/ui/add/AddScreenP.kt
+++ app/src/main/java/com/example/pamfinal/ui/add/AddScreenP.kt
@@ -3,6 +3,7 @@ package com.example.pamfinal.ui.add
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
@@ -27,7 +28,12 @@ fun EntryBody(
     onSaveClick: () -> Unit,
     modifier: Modifier = Modifier
 ){
+    Column(
+        verticalArrangement = Arrangement.spacedBy(15.dp),
+        modifier = modifier.padding(15.dp)
+    ){
 
+    }
 }
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
