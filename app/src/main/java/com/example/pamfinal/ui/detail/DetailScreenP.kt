709e11b532c6827563accdc8c27855f9fe192363
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-01-08 00:40:39 +0700

    add card

--- app/src/main/java/com/example/pamfinal/ui/detail/DetailScreenP.kt
+++ app/src/main/java/com/example/pamfinal/ui/detail/DetailScreenP.kt
@@ -2,12 +2,18 @@ package com.example.pamfinal.ui.detail
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.example.pamfinal.model.Pendaftar
 import com.example.pamfinal.navigation.DestinasiNavigasi
 
@@ -23,7 +29,15 @@ fun ItemDetailsPendaftar(
     pendaftar: Pendaftar,
     modifier: Modifier = Modifier
 ){
+    Card(
+        modifier = Modifier,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ){
 
+    }
 }
 @Composable
 private fun ItemDetailsRow(
