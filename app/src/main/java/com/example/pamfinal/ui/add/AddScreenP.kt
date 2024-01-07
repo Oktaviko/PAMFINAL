d20bbae6fc884717d6d2789ce80dca15af3115ea
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-01-07 22:22:22 +0700

    copy outlinetextfield yang dibutuhkan

--- app/src/main/java/com/example/pamfinal/ui/add/AddScreenP.kt
+++ app/src/main/java/com/example/pamfinal/ui/add/AddScreenP.kt
@@ -3,11 +3,13 @@ package com.example.pamfinal.ui.add
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.pamfinal.AddEvent
 import com.example.pamfinal.navigation.DestinasiNavigasi
@@ -32,6 +34,39 @@ fun FormInput(
             value = addEvent.nama,
             onValueChange = {onValueChange(addEvent.copy(nama = it))},
             label ={Text("Nama")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = addEvent.nik,
+            onValueChange = {onValueChange(addEvent.copy(nik = it))},
+            label ={Text("NIK")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = addEvent.alamat,
+            onValueChange = {onValueChange(addEvent.copy(alamat = it))},
+            label ={Text("Alamat")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = addEvent.tanggal_lahir,
+            onValueChange = {onValueChange(addEvent.copy(tanggal_lahir = it))},
+            label ={Text("Tanggal Lahir")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = addEvent.telepon,
+            onValueChange = {onValueChange(addEvent.copy(telepon = it))},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label ={Text("Telepon")},
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
