fef1170500d6b6018c3887a9a97def4ada85544f
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-01-08 00:32:09 +0700

    tambah alert di fungsi delete

--- app/src/main/java/com/example/pamfinal/ui/detail/DetailScreenP.kt
+++ app/src/main/java/com/example/pamfinal/ui/detail/DetailScreenP.kt
@@ -1,5 +1,8 @@
 package com.example.pamfinal.ui.detail
 
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.pamfinal.navigation.DestinasiNavigasi
@@ -16,5 +19,20 @@ private fun DeleteConfirmationDialog(
     onDeleteCancel: () -> Unit,
     modifier: Modifier = Modifier
 ){
-
+    AlertDialog(
+        onDismissRequest = { /*TODO*/ },
+        title = { Text("Are you sure?")},
+        text = { Text("Delete")},
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "No")
+            }
+        },
+        confirmButton = { 
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        }
+    )
 }
