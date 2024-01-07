cc686336a1b47c01b0245ccf8e33ff529c67ed5a
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-01-07 22:18:51 +0700

    tambah OutlinedTEXTField di dalam column

--- app/src/main/java/com/example/pamfinal/ui/add/AddScreenP.kt
+++ app/src/main/java/com/example/pamfinal/ui/add/AddScreenP.kt
@@ -2,6 +2,10 @@ package com.example.pamfinal.ui.add
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -12,6 +16,7 @@ object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = "Entry Customer"
 }
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormInput(
     addEvent: AddEvent,
@@ -23,6 +28,13 @@ fun FormInput(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(15.dp)
     ){
-
+        OutlinedTextField(
+            value = addEvent.nama,
+            onValueChange = {onValueChange(addEvent.copy(nama = it))},
+            label ={Text("Nama")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
     }
 }
