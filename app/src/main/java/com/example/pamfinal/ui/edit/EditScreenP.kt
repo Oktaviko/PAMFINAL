143d3cd51a8f6c43870204d0d93477fe5620ccee
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-01-08 01:28:21 +0700

    add topbar, dan memenaggil entrybody

--- app/src/main/java/com/example/pamfinal/ui/edit/EditScreenP.kt
+++ app/src/main/java/com/example/pamfinal/ui/edit/EditScreenP.kt
@@ -1,10 +1,20 @@
 package com.example.pamfinal.ui.edit
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pamfinal.navigation.DestinasiNavigasi
+import com.example.pamfinal.ui.BPJSTopAppBar
 import com.example.pamfinal.ui.PenyediaViewModel
+import com.example.pamfinal.ui.add.EntryBody
+import kotlinx.coroutines.launch
 
 object EditDestination : DestinasiNavigasi{
     override val route = "item_edit_pendaftar"
@@ -13,6 +23,7 @@ object EditDestination : DestinasiNavigasi{
     val routeWithArgs = "${EditDestination.route}/{$pendaftarId}"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditScreenPendaftar(
     navigateBack: () -> Unit,
@@ -20,5 +31,31 @@ fun EditScreenPendaftar(
     modifier: Modifier = Modifier,
     viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            BPJSTopAppBar(
+                title = EditDestination.titleRes,
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ){innerPadding ->
+        EntryBody(
+            addUIState = viewModel.pendaftarUiState,
+            onCustomerValueChange = viewModel::updateUIState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updatePendaftar()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
 
+    }
 }
