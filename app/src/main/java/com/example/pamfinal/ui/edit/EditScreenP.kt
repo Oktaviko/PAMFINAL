f16c0eab3322d5a1228dccf97068cdb93fe4e6ae
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-01-08 01:24:34 +0700

    Create EditScreenPendaftar

--- app/src/main/java/com/example/pamfinal/ui/edit/EditScreenP.kt
+++ app/src/main/java/com/example/pamfinal/ui/edit/EditScreenP.kt
@@ -1,10 +1,24 @@
 package com.example.pamfinal.ui.edit
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pamfinal.navigation.DestinasiNavigasi
+import com.example.pamfinal.ui.PenyediaViewModel
 
 object EditDestination : DestinasiNavigasi{
     override val route = "item_edit_pendaftar"
     override val titleRes = "Edit Pendaftar"
     const val pendaftarId = "itemId"
     val routeWithArgs = "${EditDestination.route}/{$pendaftarId}"
+}
+
+@Composable
+fun EditScreenPendaftar(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
 }
