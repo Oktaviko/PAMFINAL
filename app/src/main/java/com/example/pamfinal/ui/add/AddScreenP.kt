473f6c0e9586839e7cecedc94cbbf27db16c02ce
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-01-07 22:29:31 +0700

    tambah button di Function EntryBody

--- app/src/main/java/com/example/pamfinal/ui/add/AddScreenP.kt
+++ app/src/main/java/com/example/pamfinal/ui/add/AddScreenP.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -37,6 +39,12 @@ fun EntryBody(
             onValueChange = onCustomerValueChange,
             modifier = Modifier.fillMaxWidth()
         )
+        Button(onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+           Text("Submit")
+        }
     }
 }
 @OptIn(ExperimentalMaterial3Api::class)
