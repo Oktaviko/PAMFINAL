69d10d4c371cf0bdfed6cffc231234f6dcbf2724
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-01-07 22:47:07 +0700

    revisi Container dan buat PenyediaViewModel

---
+++ app/src/main/java/com/example/pamfinal/PenyediaViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.pamfinal
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pamfinal.ui.add.AddViewModel
+
+fun CreationExtras.aplikasiBPJS(): BPJSApplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as BPJSApplication)
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            AddViewModel(aplikasiBPJS().container.pendaftarRepository)
+        }
+    }
+}
+
