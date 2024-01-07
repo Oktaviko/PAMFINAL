4cb7c2888ff12dd8c146929730dc149ad4262469
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-01-08 01:10:05 +0700

    create initializer DetailViewModel()

--- app/src/main/java/com/example/pamfinal/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pamfinal/ui/PenyediaViewModel.kt
@@ -1,11 +1,13 @@
 package com.example.pamfinal.ui
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.pamfinal.BPJSApplication
 import com.example.pamfinal.ui.add.AddViewModel
+import com.example.pamfinal.ui.detail.DetailViewModel
 
 fun CreationExtras.aplikasiBPJS(): BPJSApplication =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as BPJSApplication)
@@ -14,6 +16,12 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
             AddViewModel(aplikasiBPJS().container.pendaftarRepository)
+        }
+        initializer {
+            DetailViewModel(
+                createSavedStateHandle(),
+                aplikasiBPJS().container.pendaftarRepository
+            )
         }
     }
 }
