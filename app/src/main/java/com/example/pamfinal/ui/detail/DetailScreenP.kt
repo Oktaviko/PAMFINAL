48be5f3d27f8cb2277e3489eef888f656b57dae6
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-01-08 00:35:07 +0700

    add row in function ItemDetailsRow

--- app/src/main/java/com/example/pamfinal/ui/detail/DetailScreenP.kt
+++ app/src/main/java/com/example/pamfinal/ui/detail/DetailScreenP.kt
@@ -1,10 +1,13 @@
 package com.example.pamfinal.ui.detail
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import com.example.pamfinal.navigation.DestinasiNavigasi
 
 object DetailDestination : DestinasiNavigasi {
@@ -19,7 +22,14 @@ private fun ItemDetailsRow(
     itemDetail: String,
     modifier: Modifier = Modifier
 ){
-
+    Row(modifier = modifier){
+        Text(text = labelResID,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = itemDetail,
+            fontWeight = FontWeight.Bold)
+    }
 }
 @Composable
 private fun DeleteConfirmationDialog(
