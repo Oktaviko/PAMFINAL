13024ec3bb4be553de0b83b24ccfaab172612737
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-01-08 21:07:07 +0700

    tambah UiState.kt untuk repo RS

--- app/src/main/java/com/example/pamfinal/ui/UiState.kt
+++ app/src/main/java/com/example/pamfinal/ui/UiState.kt
@@ -1,21 +1,30 @@
 package com.example.pamfinal.ui
 
 import com.example.pamfinal.model.Pendaftar
+import com.example.pamfinal.model.RumahSakit
 
 
-data class AddUIState(
-    val addEvent: AddEvent = AddEvent(),
+data class AddUIStatePendaftar(
+    val addEvent: AddEventPendaftar = AddEventPendaftar(),
+)
+data class AddUIStateRS(
+    val addEvent: AddEventRS = AddEventRS(),
 )
 
-data class AddEvent(
+data class AddEventPendaftar(
     val nik: String = "",
     val nama: String = "",
     val alamat: String = "",
     val telepon: String = "",
     val tanggal_lahir: String = "",
 )
+data class AddEventRS(
+    val id_rs: String = "",
+    val nama_rs: String = "",
+    val alamat_rs: String = "",
+)
 
-fun AddEvent.toPendaftar() = Pendaftar(
+fun AddEventPendaftar.toPendaftar() = Pendaftar(
     nik=nik,
     nama= nama,
     alamat= alamat,
@@ -23,25 +32,43 @@ fun AddEvent.toPendaftar() = Pendaftar(
     tanggal_lahir = tanggal_lahir,
 
 )
-
-data class DetailUIState(
-    val addEvent: AddEvent = AddEvent(),
+fun AddEventRS.toRS() = RumahSakit(
+    id_rs =id_rs,
+    nama_rs = nama_rs,
+    alamat_rs = alamat_rs,
 )
-
-fun Pendaftar.toDetailPendaftar(): AddEvent =
-    AddEvent(
+data class DetailUIStatePendaftar(
+    val addEvent: AddEventPendaftar = AddEventPendaftar(),
+)
+data class DetailUIStateRS(
+    val addEvent: AddEventRS = AddEventRS(),
+)
+fun Pendaftar.toDetailPendaftar(): AddEventPendaftar =
+    AddEventPendaftar(
         nik=nik,
         nama= nama,
         alamat= alamat,
         telepon = telepon,
         tanggal_lahir = tanggal_lahir,
     )
-
-fun Pendaftar.toUIStatePendaftar(): AddUIState = AddUIState(
+fun RumahSakit.toDetailRS(): AddEventRS =
+    AddEventRS(
+        id_rs =id_rs,
+        nama_rs = nama_rs,
+        alamat_rs = alamat_rs,
+    )
+fun Pendaftar.toUIStatePendaftar(): AddUIStatePendaftar = AddUIStatePendaftar(
     addEvent = this.toDetailPendaftar()
 )
+fun RumahSakit.toUIStateRS(): AddUIStateRS = AddUIStateRS(
+    addEvent = this.toDetailRS()
+)
 
-data class HomeUIState(
+data class HomeUIStatePendaftar(
+    val listPendaftar: List<Pendaftar> = listOf(),
+    val dataLength: Int = 0
+)
+data class HomeUIStateRS(
     val listPendaftar: List<Pendaftar> = listOf(),
     val dataLength: Int = 0
 )
