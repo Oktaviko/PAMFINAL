7cffcb8a8da6597c9e433927700b996c429b6dee
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-01-11 19:42:56 +0700

    edit repo

--- app/src/main/java/com/example/pamfinal/data/Repository.kt
+++ app/src/main/java/com/example/pamfinal/data/Repository.kt
@@ -124,7 +124,7 @@ interface KartuRepository {
 class KartuRepositoryImpl(private val firestore: FirebaseFirestore) : KartuRepository {
     override fun getAll(): Flow<List<Kartu>> = flow {
         val snapshot = firestore.collection("Kartu")
-            .orderBy("id_rs", Query.Direction.ASCENDING)
+            .orderBy("id_kartu", Query.Direction.ASCENDING)
             .get()
             .await()
         val kartu = snapshot.toObjects(Kartu::class.java)
