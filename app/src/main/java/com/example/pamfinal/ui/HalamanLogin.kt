4f9f4461e1be80163ef67213c4db5f21d2cded96
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-01-11 19:51:27 +0700

    add setting column

--- app/src/main/java/com/example/pamfinal/ui/HalamanLogin.kt
+++ app/src/main/java/com/example/pamfinal/ui/HalamanLogin.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -53,7 +54,9 @@ fun LoginScreen(
     var passwordText by remember { mutableStateOf("") }
 
     Column(
-
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
     ){
 
     }
