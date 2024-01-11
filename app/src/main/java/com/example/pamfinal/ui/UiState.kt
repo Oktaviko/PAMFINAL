98ea44c341cb390240c415eb1082a64f15129bd3
Oktaviko <115150122+Oktaviko@users.noreply.github.com>
2024-01-11 10:41:23 +0700

    add uistate kartu

--- app/src/main/java/com/example/pamfinal/ui/UiState.kt
+++ app/src/main/java/com/example/pamfinal/ui/UiState.kt
@@ -1,5 +1,6 @@
 package com.example.pamfinal.ui
 
+import com.example.pamfinal.model.Kartu
 import com.example.pamfinal.model.Pendaftar
 import com.example.pamfinal.model.RumahSakit
 
@@ -9,6 +10,9 @@ data class AddUIStatePendaftar(
 )
 data class AddUIStateRumahSakit(
     val addEventRumahSakit: AddEventRumahSakit = AddEventRumahSakit(),
+)
+data class AddUIStateKartuBpjs(
+    val addEventKartuBpjs: AddEventKartuBpjs = AddEventKartuBpjs(),
 )
 
 data class AddEventPendaftar(
@@ -23,6 +27,12 @@ data class AddEventRumahSakit(
     val nama_rs: String = "",
     val alamat_rs: String = "",
 )
+data class AddEventKartuBpjs(
+    val id_kartu: String = "",
+    val pendaftar: String = "",
+    val rumahSakit: String = "",
+    val masa_aktif: String = "",
+)
 
 fun AddEventPendaftar.toPendaftar() = Pendaftar(
     nik=nik,
@@ -36,11 +46,21 @@ fun AddEventRumahSakit.toRumahSakit() = RumahSakit(
     nama_rs = nama_rs,
     alamat_rs = alamat_rs,
 )
+fun AddEventKartuBpjs.toKartuBpjs() = Kartu(
+    id_kartu =id_kartu,
+    pendaftar = pendaftar,
+    rumahSakit = rumahSakit,
+    masa_Aktif = masa_aktif,
+)
+
 data class DetailUIStatePendaftar(
     val addEventPendaftar: AddEventPendaftar = AddEventPendaftar(),
 )
 data class DetailUIStateRumahSakit(
     val addEventRumahSakit: AddEventRumahSakit = AddEventRumahSakit(),
+)
+data class DetailUIStateKartuBpjs(
+    val addEventKartuBpjs: AddEventKartuBpjs = AddEventKartuBpjs(),
 )
 fun Pendaftar.toDetailPendaftar(): AddEventPendaftar =
     AddEventPendaftar(
@@ -56,11 +76,21 @@ fun RumahSakit.toDetailRumahSakit(): AddEventRumahSakit =
         nama_rs = nama_rs,
         alamat_rs = alamat_rs,
     )
+fun Kartu.toDetailKartu(): AddEventKartuBpjs =
+    AddEventKartuBpjs(
+        id_kartu = id_kartu,
+        pendaftar = pendaftar,
+        rumahSakit = rumahSakit,
+        masa_aktif = masa_Aktif,
+    )
 fun Pendaftar.toUIStatePendaftar(): AddUIStatePendaftar = AddUIStatePendaftar(
     addEventPendaftar = this.toDetailPendaftar()
 )
 fun RumahSakit.toUIStateRumahSakit(): AddUIStateRumahSakit = AddUIStateRumahSakit(
     addEventRumahSakit = this.toDetailRumahSakit()
+)
+fun Kartu.toUIStateKartuBpjs(): AddUIStateKartuBpjs = AddUIStateKartuBpjs(
+    addEventKartuBpjs = this.toDetailKartu()
 )
 
 data class HomeUIStatePendaftar(
@@ -69,5 +99,9 @@ data class HomeUIStatePendaftar(
 )
 data class HomeUIStateRumahSakit(
     val listRumahSakit: List<RumahSakit> = listOf(),
+    val dataLength: Int = 0
+)
+data class HomeUIStateKartu(
+    val listPendaftar: List<Kartu> = listOf(),
     val dataLength: Int = 0
 )
